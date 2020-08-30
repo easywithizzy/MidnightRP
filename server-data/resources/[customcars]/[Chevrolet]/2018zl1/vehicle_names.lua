a573db0550e26e631b513407864771ccc74b985d
TJ Spann <tj@tspann.us>
2020-08-29 22:13:17 -0600

    Cars!

---
+++ server-data/resources/[customcars]/[Chevrolet]/2018zl1/vehicle_names.lua
@@ -0,0 +1,3 @@
+Citizen.CreateThread(function()
+    AddTextEntry("2018zl1","2018 Chevrolet Camaro ZL1")
+end)
---
+++ server-data/resources/[customcars]/[Dodge]/demon/vehicle_names.lua
@@ -0,0 +1,3 @@
+Citizen.CreateThread(function()
+    AddTextEntry("demon","2018 Dodge Challenger SRT Demon")
+end)
