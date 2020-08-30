0c65b7c18c5ce6246b1c97076d1d8902aedb6ad3
TJ Spann <tj@tspann.us>
2020-08-30 12:42:17 -0600

    .

--- server-data/resources/[esx]/esx_unicornjob/esx_unicornjob.sql
+++ server-data/resources/[esx]/esx_unicornjob/esx_unicornjob.sql
@@ -2,12 +2,6 @@ SET @job_name = 'unicorn';
 SET @society_name = 'society_unicorn';
 SET @job_Name_Caps = 'Unicorn';
 
-
-
-INSERT INTO `addon_account` (name, label, shared) VALUES
-  (@society_name, @job_Name_Caps, 1)
-;
-
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
   (@society_name, @job_Name_Caps, 1),
   ('society_unicorn_fridge', 'Unicorn (frigo)', 1)
