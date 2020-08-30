d69b58533bc522809eca6129588941a40686036b
TJ Spann <tj@tspann.us>
2020-08-30 12:51:14 -0600

    .

--- server-data/resources/[esx]/esx_repairkit/locales/en.lua
+++
@@ -1,8 +0,0 @@
-Locales ['en'] = {
-	['used_kit']					= 'You used ~y~x1 ~b~Repairkit',
-	['must_be_outside']				= 'You must be outside of the vehicle!',
-	['no_vehicle_nearby']			= 'There is no ~r~vehicle ~w~nearby',
-	['finished_repair']				= '~g~You repaired the vehicle!',
-	['abort_hint']					= 'Press ~INPUT_VEH_DUCK~ to cancel',
-	['aborted_repair']				= 'You ~r~aborted ~w~the repairs',
-}
